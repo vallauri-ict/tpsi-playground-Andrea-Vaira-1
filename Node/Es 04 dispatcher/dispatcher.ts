a1ade85868e43d63e2115e4c539cd68ec6564a58
Andrea Vaira <andrea.vaira@cambieri.it>
2022-10-06 09:46:48 +0200

    Start creation dispatcher

--- Node/Es 04 dispatcher/dispatcher.ts
+++ Node/Es 04 dispatcher/dispatcher.ts
@@ -2,11 +2,13 @@ import _http from "http";
 import _url from "url";
 import _fs from "fs";
 import _mime from "mime";
-import _queryString from "querystring";
+import _queryString from "query-string";
+import _header from "./headers.json";
+import { json } from "stream/consumers";
 
 let paginaErrore: string;
 
-export class Dispatcher {
+class Dispatcher {
   prompt: string = ">>> ";
   listeners: any = {
     GET: {
@@ -21,16 +23,64 @@ export class Dispatcher {
     init();
   }
 
-  addListener(method: string, resource: string, callback: any) {
+  public addListener(method: string, resource: string, callback: any) {
     method = method.toUpperCase();
     if (method in this.listeners) {
+      //Registrazione del nuovo listener
       this.listeners[method][resource] = callback;
     } else {
       throw new Error("Metodo non valido");
     }
   }
-}
 
+  public dispatch(req: any, res: any) {
+    let method: string = req.method.toUpperCase();
+    if (method == "GET") {
+      this.innerDispatch(req, res);
+    } else {
+      let parameters: string = "";
+      req.on("data", (data: any) => {
+        parameters += data;
+      });
+      req.on("end", () => {
+        let json;
+        try {
+          json = JSON.stringify(parameters);
+        } catch (error) {
+          json = _queryString.parse(parameters);
+        }
+      });
+    }
+  }
+
+  private innerDispatch(req: any, res: any) {
+    let method: string = req.method.toUpperCase();
+    let url: any = _url.parse(req.url, true);
+    let resource: string = url.pathname;
+    let parameters: any = url.query;
+
+    console.log(
+      this.prompt + method + ":" + resource + " " + JSON.stringify(parameters)
+    );
+
+    req.GET = parameters;
+
+    if (!resource.startsWith("/api/")) {
+      staticiListener(req, res, resource);
+    } else {
+      if (method in this.listeners && resource in this.listeners[method]) {
+        // this.listeners[method][resource]();  va in esecuzione la callback corrispondente
+        // stessa cosa ma su due righe
+        let callback = this.listeners[method][resource];
+        callback(req, res);
+      } else {
+        res.writeHead(404, _header.text);
+        res.write("Servizio non disponibile");
+        res.end();
+      }
+    }
+  }
+}
 let init = () => {
   _fs.readFile("./static/error.html", (err, data) => {
     if (!err) {
@@ -40,3 +90,24 @@ let init = () => {
     }
   });
 };
+let staticiListener = (req: any, res: any, resource: string) => {
+  if (resource == "/") {
+    resource = "/index.html";
+  }
+  let filename = "./static" + resource;
+  _fs.readFile(filename, (err: any, data: any) => {
+    if (!err) {
+      let header = { "Content-Type": _mime.getType(filename) };
+      res.writeHead(200, header);
+      res.write(data);
+      res.end();
+    } else {
+      res.writeHead(404, _header.html);
+      res.write(paginaErrore);
+      res.end();
+    }
+  });
+};
+
+// Creiamo ed esportiamo l'istanza
+export default new Dispatcher();
---
+++ Node/Es 04 dispatcher/server.ts
@@ -0,0 +1,31 @@
+import _http from "http";
+import _url from "url";
+import _fs from "fs";
+import _dispatcher from "./dispatcher";
+import _headers from "./headers.json";
+
+const PORT = 1337;
+
+let server = _http.createServer((req, res) => {
+  // Cerca il listener opportuno e lo manda in esecuzione
+  _dispatcher.dispatch(req, res);
+});
+
+server.listen(PORT, () => {
+  console.log("Server in ascolto sulla porta " + PORT);
+});
+
+/*******CREAZIONE DEI LISTENER******/
+_dispatcher.addListener("GET", "/api/servizio1", (req: any, res: any) => {
+  res.writeHead(200, _headers.json);
+  let data = { ris: "Benvenuto " + req.GET.nome };
+  res.write(JSON.stringify(data));
+  res.end();
+});
+
+_dispatcher.addListener("POST", "/api/servizio2", (req: any, res: any) => {
+  res.writeHead(200, _headers.json);
+  let data = { ris: "servizio2 ok" };
+  res.write(JSON.stringify(data));
+  res.end();
+});
