b00b21f48030036c74f6c7fa34d69ceb5df23b35
Andrea Vaira <andrea.vaira@cambieri.it>
2022-12-19 18:08:42 +0100

    Controllo pre verifica

--- Angular/example-project/src/app/app.module.ts
+++ Angular/example-project/src/app/app.module.ts
@@ -3,16 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './navbar/navbar.component';
-import { FooterComponent } from './footer/footer.component';
-import { CardComponent } from './card/card.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    NavbarComponent,
-    FooterComponent,
-    CardComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
