a90d5b252c86b4e60c630d9cddf63a6c57b4e83a
Andrea Vaira <andrea.vaira@cambieri.it>
2023-03-16 18:37:26 +0100

    Big data project bug fixing done

--- BigData/bigData/static/index.js
+++ BigData/bigData/static/index.js
@@ -1,47 +1,7 @@
 "use strict";
 
 $(document).ready(function () {
-  let easing = helpers.easingEffects.easeOutQuad;
-  let restart = false;
-  const totalDuration = 1000;
-  const duration = (ctx) =>
-    (easing(ctx.index / data.length) * totalDuration) / data.length;
-  const delay = (ctx) => easing(ctx.index / data.length) * totalDuration;
-  const previousY = (ctx) =>
-    ctx.index === 0
-      ? ctx.chart.scales.y.getPixelForValue(100)
-      : ctx.chart
-          .getDatasetMeta(ctx.datasetIndex)
-          .data[ctx.index - 1].getProps(["y"], true).y;
-  const animation = {
-    x: {
-      type: "number",
-      easing: "linear",
-      duration: duration,
-      from: NaN, // the point is initially skipped
-      delay(ctx) {
-        if (ctx.type !== "data" || ctx.xStarted) {
-          return 0;
-        }
-        ctx.xStarted = true;
-        return delay(ctx);
-      },
-    },
-    y: {
-      type: "number",
-      easing: "linear",
-      duration: duration,
-      from: previousY,
-      delay(ctx) {
-        if (ctx.type !== "data" || ctx.yStarted) {
-          return 0;
-        }
-        ctx.yStarted = true;
-        return delay(ctx);
-      },
-    },
-  };
-
+  
 
   const config = {
     type: "line",
@@ -62,10 +22,6 @@ $(document).ready(function () {
       ],
     },
     options: {
-      animation,
-      interaction: {
-        intersect: false,
-      },
       responsive: true,
       plugins: {
         legend: {
