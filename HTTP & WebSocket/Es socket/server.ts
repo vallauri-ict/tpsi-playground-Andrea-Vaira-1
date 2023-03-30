86b052e0ee96d11b3658e5fb9c3d2752aad71a5d
Andrea Vaira <andrea.vaira@cambieri.it>
2023-03-30 09:44:27 +0200

    Go on with web socket excercise

--- HTTP & WebSocket/Es socket/server.ts
+++ HTTP & WebSocket/Es socket/server.ts
@@ -4,11 +4,11 @@ import url from "url";
 import fs from "fs";
 import dotenv from "dotenv";
 import { MongoClient, ObjectId } from "mongodb";
-import express from "express";  // @types/express
-import cors from "cors"         // @types/cors
+import express from "express"; // @types/express
+import cors from "cors"; // @types/cors
 import fileUpload, { UploadedFile } from "express-fileupload";
-import {Server, Socket} from 'socket.io'; 
-import colors from 'colors';
+import { Server, Socket } from "socket.io";
+import colors from "colors";
 
 // configurazioni
 dotenv.config({ path: ".env" });
@@ -17,27 +17,26 @@ const httpServer = http.createServer(app);
 const io = new Server(httpServer);
 const PORT = process.env.PORT || 1337;
 const DB_NAME = "5b";
-const connectionString:any = process.env.connectionString;
+const connectionString: any = process.env.connectionString;
 
 httpServer.listen(PORT, function () {
-	console.log("Server in ascolto sulla porta " + PORT)
-	init();
-  });
-  
-let paginaErrore = "";
-function init() {
-fs.readFile("./static/error.html", function (err, data) {
-  if (!err) {
-	paginaErrore = data.toString();
-  }
-  else {
-	paginaErrore = "<h2>Risorsa non trovata</h2>";
-  }
+  console.log("Server in ascolto sulla porta " + PORT);
+  init();
 });
 
-/********************************* Middleware ************************* */
-  }
-  
+let paginaErrore = "";
+function init() {
+  fs.readFile("./static/error.html", function (err, data) {
+    if (!err) {
+      paginaErrore = data.toString();
+    } else {
+      paginaErrore = "<h2>Risorsa non trovata</h2>";
+    }
+  });
+
+  /********************************* Middleware ************************* */
+}
+
 // 1 request log
 app.use("/", (req: any, res: any, next: any) => {
   console.log(req.method + ": " + req.originalUrl);
@@ -47,124 +46,153 @@ app.use("/", (req: any, res: any, next: any) => {
 // 2 gestione delle risorse statiche
 app.use("/", express.static("./static"));
 
-
 // 3 lettura dei parametri POST
 // Il limit serve per upload base64
 // da express 4.16 (oggi 2022.4.18)
-app.use("/", express.json({"limit":"50mb"}))
-app.use("/", express.urlencoded({"limit":"50mb", "extended": true }))
-
+app.use("/", express.json({ limit: "50mb" }));
+app.use("/", express.urlencoded({ limit: "50mb", extended: true }));
 
 // 4 log dei parametri get e post
 app.use("/", (req: any, res: any, next: any) => {
   if (Object.keys(req.query).length != 0) {
-	  console.log("------> Parametri GET: " + JSON.stringify(req.query));
+    console.log("------> Parametri GET: " + JSON.stringify(req.query));
   }
   if (Object.keys(req.body).length != 0) {
-	  console.log("------> Parametri BODY: " +JSON.stringify(req.body));
+    console.log("------> Parametri BODY: " + JSON.stringify(req.body));
   }
   next();
 });
 
-
 // 6 - CORS Policy
-const whitelist = [	];
+const whitelist = [];
 const corsOptions = {
-    origin: function(origin:any, callback:any) {
-          return callback(null, true);
-    },
-    credentials: true
+  origin: function (origin: any, callback: any) {
+    return callback(null, true);
+  },
+  credentials: true,
 };
 app.use("/", cors(corsOptions));
 
-
 // 7 - per far sì che i json restituiti al client abbiano indentazione 4 chr
-app.set("json spaces", 4)
-
+app.set("json spaces", 4);
 
 // 10 apertura della connessione
-app.use("/api/", function (req:any, res, next) {
-	let connection = new MongoClient(connectionString);
-    connection.connect()
-	.then((client: any) => {
-		// req["client"] è un campo esistente !!!!!!!!!!
-		req["connessione"]=client;
-		next();
-	})
-	.catch((err: any) => {
-		let msg = "Errore di connessione al db"
-		res.status(503).send(msg)
-	}) 
-})
-
+app.use("/api/", function (req: any, res, next) {
+  let connection = new MongoClient(connectionString);
+  connection
+    .connect()
+    .then((client: any) => {
+      // req["client"] è un campo esistente !!!!!!!!!!
+      req["connessione"] = client;
+      next();
+    })
+    .catch((err: any) => {
+      let msg = "Errore di connessione al db";
+      res.status(503).send(msg);
+    });
+});
 
 /********************************** REST SERVICES *************************** */
 
-app.get('/api/utenti', function(req:any, res, next) {
-	let collection = req["connessione"].db(DB_NAME).collection("images");
-	// collection.find({'occupato':false}).toArray(function(err:any, data:any) {
-	// 	if (err) {
-	// 		res.status(500).send("Errore esecuzione query")
-	// 	} 
-	// 	else {
-	// 		res.send(data);
-	// 	}
-	// 	req["connessione"].close();
-	// })
+app.get("/api/utenti", function (req: any, res, next) {
+  let collection = req["connessione"].db(DB_NAME).collection("images");
+  // collection.find({'occupato':false}).toArray(function(err:any, data:any) {
+  // 	if (err) {
+  // 		res.status(500).send("Errore esecuzione query")
+  // 	}
+  // 	else {
+  // 		res.send(data);
+  // 	}
+  // 	req["connessione"].close();
+  // })
 
-	// Gestione con la promise
-	collection
-	.find({ occupato: false })
-	.toArray()
-	.then((data:any[])=>{
-		res.send(data);
-	})
-	.catch((err:Error)=>{
-		res.status(500).send("Errore esecuzione query");
-		console.log('Errore: '+err);
-	})
-	.finally(()=>{
-		req["connessione"].close();
-	})
-}) 
-
-
-
+  // Gestione con la promise
+  collection
+    .find({ occupato: false })
+    .toArray()
+    .then((data: any[]) => {
+      res.send(data);
+    })
+    .catch((err: Error) => {
+      res.status(500).send("Errore esecuzione query");
+      console.log("Errore: " + err);
+    })
+    .finally(() => {
+      req["connessione"].close();
+    });
+});
 
 // Default route
-app.use('/', function (req:any, res, next) {
-    res.status(404)
-    if (req.originalUrl.startsWith("/api/")) {
-        res.send("Risorsa non trovata");
-		req["connessione"].close();
-    }
-    else  
-		res.send(paginaErrore);
+app.use("/", function (req: any, res, next) {
+  res.status(404);
+  if (req.originalUrl.startsWith("/api/")) {
+    res.send("Risorsa non trovata");
+    req["connessione"].close();
+  } else res.send(paginaErrore);
 });
 
 // Gestione degli errori
 app.use("/", (err: any, req: any, res: any, next: any) => {
-	console.log(req["connessione"]);
-	if(req["connessione"]) req["connessione"].close();
-    console.log("SERVER ERROR " + err.stack);
-    res.status(500);
-    res.send("ERRR: " + err.message);
+  console.log(req["connessione"]);
+  if (req["connessione"]) req["connessione"].close();
+  console.log("SERVER ERROR " + err.stack);
+  res.status(500);
+  res.send("ERRR: " + err.message);
 });
-
-
-
 
 /******************************* gestione web socket *************************** */
-let users:any[] = [];
+let users: any[] = [];
 
-io.on('connection', function (clientSocket:Socket) {
-	let user = {} as {username:string, room:string, socket:Socket};
-	clientSocket.on('joinRoom', (clientUser)=>{
+io.on("connection", function (clientSocket: Socket) {
+  let user: any = {};
 
-	})
+  clientSocket.on("joinRoom", async (clientUser) => {
+    user = JSON.parse(clientUser);
+    user.socket = clientSocket;
+    console.log("User " + user.username + " is connected");
+    users.push(user); // non è necessaria in questo caso
+    await aggiornaDb(user._id, true);
+    clientSocket.join(user.room); // aggiungo l'utente alla stanza selezionata
+    clientSocket.emit("joinRoomAck", "ok");
+  });
 
+  clientSocket.on("newMessage", (msg) => {
+    console.log(`User ${user.username} ha inviato il messaggio ${msg}`);
+    let response = {
+      from: user.username,
+      message: msg,
+      img: user.img,
+      date: new Date(),
+    };
+    io.to(user.room).emit("notifyMessage", JSON.stringify(response));
+  });
 
+  clientSocket.on("disconnect", async () => {
+    users.splice(users.indexOf(user), 1);
+    await aggiornaDb(user._id, false);
+    console.log("User " + user.username + " disconnected");
+  });
 });
 
-
-
+async function aggiornaDb(id: any, stato: Boolean) {
+  id = new ObjectId(id);
+  let connection = new MongoClient(connectionString);
+  await connection
+    .connect()
+    .then((client: any) => {
+      let collection = client.db(DB_NAME).collection("images");
+      collection
+        .updateOne({ _id: id }, { $set: { occupato: stato } })
+        .then()
+        .catch(() => {
+          console.log("Errore esecuzione query");
+        })
+        .finally(() => {
+          client.close();
+        });
+    })
+    .catch((err: any) => {
+      let msg = "Errore di connessione al db";
+      console.log(msg);
+    });
+}
--- HTTP & WebSocket/Es socket/static/index.js
+++ HTTP & WebSocket/Es socket/static/index.js
@@ -32,6 +32,7 @@ $(document).ready(function () {
     let request = inviaRichiesta("GET", "/api/utenti");
     request.then((response) => {
       let users = response.data;
+      divUtenti.empty();
       for (const user of users) {
         $("<label>")
           .addClass("form-check-label")
@@ -45,6 +46,7 @@ $(document).ready(function () {
               .addClass("form-check-input")
               .on("click", function () {
                 selectedUser.text(user.username);
+                selectedUser.prop("user", user);
               })
           )
           .append($("<span>").text(user.username));
@@ -70,14 +72,73 @@ $(document).ready(function () {
   // ******************* SOCKET *************************
   let serverSocket;
   $("#btnConnetti").on("click", function () {
+    let user = selectedUser.prop("user");
+    user.room = selectedRoom.text();
+
     let options = { transports: ["websocket"], upgrade: false };
+    // Invia una richiesta di connessione al server dal quale è stata scaricata la pagina
     serverSocket = io(options).connect();
+
     serverSocket.on("connect", () => {
-      let user = {
-        username: selectedUser.text(),
-        room: selectedRoom.text(),
-      };
+      console.log(user);
       serverSocket.emit("joinRoom", JSON.stringify(user));
     });
+
+    serverSocket.on("joinRoomAck", (data) => {
+      if (data == "ok") {
+        document.title = user.username;
+        btnConnetti.prop("disabled", true);
+        btnInvia.prop("disabled", false);
+        btnDisconnetti.prop("disabled", false);
+        wrapper.empty();
+      }
+    });
+
+    serverSocket.on("notifyMessage", (response) => {
+      response = JSON.parse(response);
+      visualizza(response);
+    });
   });
+
+  btnInvia.on("click", () => {
+    let msg = $("#txtMessage").val();
+    serverSocket.emit("newMessage", msg);
+  });
+
+  btnDisconnetti.on('click', ()=>{
+    serverSocket.disconnect();
+    btnConnetti.prop('disabled', false);
+    btnDisconnetti.prop("disabled", true);
+    btnInvia.prop("disabled", true);
+  })
+
+  function visualizza(data) {
+    let container = $("<div class='message-container'></div>");
+    container.appendTo(wrapper);
+
+    // username e date
+    let date = new Date(data.date);
+    let mittente = $(
+      "<img src='img/"+data.img +"'>"+
+      "<small class='message-from'>" +
+        data.from +
+        " @" +
+        date.toLocaleTimeString() +
+        "</small>"
+    );
+    mittente.appendTo(container);
+
+    // messaggio
+    let message = $("<p class='message-data'>" + data.message + "</p>");
+    message.appendTo(container);
+
+    // auto-scroll dei messaggi
+    /* la proprietà html scrollHeight rappresenta l'altezza di wrapper oppure
+	l'altezza del testo interno qualora questo ecceda l'altezza di wrapper
+	*/
+    let h = wrapper.prop("scrollHeight");
+    // fa scorrere il testo verso l'alto in 500ms
+    wrapper.animate({ scrollTop: h }, 500);
+    $("#txtMessage").val("");
+  }
 });
